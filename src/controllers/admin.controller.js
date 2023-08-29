27c37688c3a35a44590194872f82d8d370f697fa
quochuy <quochuy@quochuy.local>
2023-08-29 10:48:15 +0700

    update

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -4,13 +4,14 @@ exports.login = (req, res) => {
   try {
     console.log("===token", req.session.token);
     if (req.session.token) {
-      res.redirect("/dashboard", {
+      res.redirect("/dashboard");
+    } else {
+      res.render('./admin/login', {
         host: Config.host
       });
     }
-    res.render('./admin/login', {
-      host: Config.host
-    });
+
+
   } catch (err) {
     console.error(err);
     res.status(500).send('Server error');
